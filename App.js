c71a185f261a1a0d142bd2b307023a7a57785bd8
Max <maximus66@hotmail.co.uk>
2024-12-17 13:13:31 +0000

    tidying

--- App.js
+++ App.js
@@ -1,65 +1,20 @@
-import { useState } from "react";
-import { StyleSheet, ImageBackground, SafeAreaView } from "react-native";
-import { SafeAreaView as SafeAreaViewAndroid} from "react-native-safe-area-context";
-import {LinearGradient} from 'expo-linear-gradient'
-import Colors from "./constants/colors.js";
-
-import StartGameScreen from "./screens/StartGameScreen.js";
-import GameScreen from "./screens/GameScreen.js";
-import GameOverScreen from "./screens/GameOverScreen.js";
-
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const [userNumber, setUserNumber] = useState();
-  const [gameOver, setGameOver] = useState(true);
-  const [guessRounds, setGuessRounds] = useState(0)
-
-  function pickedNumberHandler(pickedNumber) {
-    setUserNumber(pickedNumber);
-    setGameOver(false);
-  }
-
-  function gameOverHandler(numberOfRounds) {
-    setGameOver(true)
-    setGuessRounds(numberOfRounds)
-  }
-
-  function startNewGameHandler(){
-    setUserNumber(null);
-    setGuessRounds(0);
-  }
-
-  let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />;
-
-  if (userNumber) {
-    screen = <GameScreen userNumber={userNumber} onGameOver={gameOverHandler}/>;
-  }
-
-  if (gameOver && userNumber) {
-    screen = <GameOverScreen userNumber={userNumber} roundsNumber={guessRounds} onStartNewGame={startNewGameHandler}/>
-  }
-
-
-
   return (
-    <LinearGradient colors={[Colors.primary500, Colors.highlight]} style={styles.rootScreen}>
-      <ImageBackground
-        source={require("./assets/background.png")}
-        resizeMode="cover"
-        style={styles.rootScreen}
-        imageStyle={styles.backgroundImage}
-      >
-        <SafeAreaViewAndroid style={styles.rootScreen}>{screen}</ SafeAreaViewAndroid>
-      </ImageBackground>
-    </LinearGradient>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  rootScreen: {
+  container: {
     flex: 1,
-  },
-  backgroundImage: {
-    opacity: 0.15,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
